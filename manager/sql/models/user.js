3a6c0c2d5dd7e739fdc553affd7177b040c37b9b
Victor Hirumitsu <vmhirumitsu@gmail.com>
2018-10-24 09:29:42 -0300

    Mudança nas models, atualização do .gitignore

---
+++ manager/sql/models/user.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    discordId: {
+      type: DataTypes.STRING,
+    },
+  });
+};

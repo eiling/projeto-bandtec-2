7a4c905e578074502366325cfb9562ba810b2723
Victor Hirumitsu <vmhirumitsu@gmail.com>
2018-10-25 00:39:23 -0300

    Removida a chave do código fonte.

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from secrets.secret_key import SECRET_KEY as KEY
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_f#ik64-dkt5m-f$5fgrsjd7q9zi8fcdq%yloqyhm!_#jrxlgd'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

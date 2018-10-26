786d38e678630ccd413f784656292578e2de7f68
Victor Hirumitsu <36802725+eiling@users.noreply.github.com>
2018-10-26 08:49:07 -0300

    Arrumar espaçamento

--- manager/sql/models.js
+++ manager/sql/models.js
@@ -7,7 +7,7 @@ const User = sequelize.import(__dirname + '\\models\\user.js');
 const Computer = sequelize.import(__dirname + '\\models\\computer.js');
 const History = sequelize.import(__dirname + '\\models\\history.js');
 
-Computer.hasMany(History,{foreignKey:'id_computer'});
+Computer.hasMany(History, {foreignKey:'id_computer'});
 User.hasMany(Computer, {foreignKey: 'id_user'});
 
 sequelize.sync({force: true}).then(() => User.build({
